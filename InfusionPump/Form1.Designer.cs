9bc0f66fbaab436e8b1595a35b311ad2eec8d91c
nzgamer41 <pslover9@gmail.com>
2021-09-20 01:30:44 +1200

    Initial commit

---
+++ InfusionPump/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace InfusionPump
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonPlus = new System.Windows.Forms.Button();
+            this.buttonMinus = new System.Windows.Forms.Button();
+            this.buttonNegAns = new System.Windows.Forms.Button();
+            this.buttonPositiveAns = new System.Windows.Forms.Button();
+            this.buttonOnOff = new System.Windows.Forms.Button();
+            this.textBoxInfo = new System.Windows.Forms.TextBox();
+            this.textBoxMode = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonPlus
+            // 
+            this.buttonPlus.Location = new System.Drawing.Point(12, 193);
+            this.buttonPlus.Name = "buttonPlus";
+            this.buttonPlus.Size = new System.Drawing.Size(95, 104);
+            this.buttonPlus.TabIndex = 0;
+            this.buttonPlus.Text = "+";
+            this.buttonPlus.UseVisualStyleBackColor = true;
+            this.buttonPlus.Click += new System.EventHandler(this.buttonPlus_Click);
+            // 
+            // buttonMinus
+            // 
+            this.buttonMinus.Location = new System.Drawing.Point(12, 303);
+            this.buttonMinus.Name = "buttonMinus";
+            this.buttonMinus.Size = new System.Drawing.Size(95, 104);
+            this.buttonMinus.TabIndex = 1;
+            this.buttonMinus.Text = "-";
+            this.buttonMinus.UseVisualStyleBackColor = true;
+            this.buttonMinus.Click += new System.EventHandler(this.buttonMinus_Click);
+            // 
+            // buttonNegAns
+            // 
+            this.buttonNegAns.Location = new System.Drawing.Point(113, 303);
+            this.buttonNegAns.Name = "buttonNegAns";
+            this.buttonNegAns.Size = new System.Drawing.Size(283, 104);
+            this.buttonNegAns.TabIndex = 2;
+            this.buttonNegAns.Text = "but";
+            this.buttonNegAns.UseVisualStyleBackColor = true;
+            this.buttonNegAns.Click += new System.EventHandler(this.buttonNegAns_Click);
+            // 
+            // buttonPositiveAns
+            // 
+            this.buttonPositiveAns.Location = new System.Drawing.Point(113, 193);
+            this.buttonPositiveAns.Name = "buttonPositiveAns";
+            this.buttonPositiveAns.Size = new System.Drawing.Size(283, 104);
+            this.buttonPositiveAns.TabIndex = 3;
+            this.buttonPositiveAns.Text = "Yes/Start";
+            this.buttonPositiveAns.UseVisualStyleBackColor = true;
+            this.buttonPositiveAns.Click += new System.EventHandler(this.buttonPositiveAns_Click);
+            // 
+            // buttonOnOff
+            // 
+            this.buttonOnOff.Location = new System.Drawing.Point(402, 193);
+            this.buttonOnOff.Name = "buttonOnOff";
+            this.buttonOnOff.Size = new System.Drawing.Size(210, 214);
+            this.buttonOnOff.TabIndex = 4;
+            this.buttonOnOff.Text = "Power On/Off";
+            this.buttonOnOff.UseVisualStyleBackColor = true;
+            this.buttonOnOff.Click += new System.EventHandler(this.executeOnOff);
+            // 
+            // textBoxInfo
+            // 
+            this.textBoxInfo.AcceptsReturn = true;
+            this.textBoxInfo.AcceptsTab = true;
+            this.textBoxInfo.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxInfo.Location = new System.Drawing.Point(12, 63);
+            this.textBoxInfo.Multiline = true;
+            this.textBoxInfo.Name = "textBoxInfo";
+            this.textBoxInfo.ReadOnly = true;
+            this.textBoxInfo.Size = new System.Drawing.Size(600, 124);
+            this.textBoxInfo.TabIndex = 5;
+            this.textBoxInfo.WordWrap = false;
+            // 
+            // textBoxMode
+            // 
+            this.textBoxMode.Location = new System.Drawing.Point(12, 34);
+            this.textBoxMode.Name = "textBoxMode";
+            this.textBoxMode.ReadOnly = true;
+            this.textBoxMode.Size = new System.Drawing.Size(600, 23);
+            this.textBoxMode.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.textBoxMode);
+            this.Controls.Add(this.textBoxInfo);
+            this.Controls.Add(this.buttonOnOff);
+            this.Controls.Add(this.buttonPositiveAns);
+            this.Controls.Add(this.buttonNegAns);
+            this.Controls.Add(this.buttonMinus);
+            this.Controls.Add(this.buttonPlus);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonPlus;
+        private System.Windows.Forms.Button buttonMinus;
+        private System.Windows.Forms.Button buttonNegAns;
+        private System.Windows.Forms.Button buttonPositiveAns;
+        private System.Windows.Forms.Button buttonOnOff;
+        private System.Windows.Forms.TextBox textBoxInfo;
+        private System.Windows.Forms.TextBox textBoxMode;
+    }
+}
+
---
+++ InfusionPump/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InfusionPump
+{
+    public partial class Form1 : Form
+    {
+        private static int VOLUME_MAX = 800; //maximum of volume, ml
+        private static int VOLUME_MIN = 100;
+        private static int DURATION_MAX = 80; //maximum of duration, mins
+        private static int DURATION_MIN = 10;
+        private static int VOLUME_STEP = 100; //increment or decrement when change volume
+        private static int DURATION_STEP = 10; //increment or decrement when change duration
+
+        private int pumpID; //default value is 1
+        private int batteryPercent = 0; // value is 100 when power on
+        private int volume = 0;
+        private int duration = 0;
+        private int oldVolume = 0; //used when cancel settings
+        private int oldDuration = 0; //used when cancel settings
+        private string displayContent = " ";
+
+        enum Status
+        {
+            Off, // power off
+            Initial, // initial status after powered on or stop infusion
+            SetVolume,
+            SetDuration,
+            SettingsConfirmed,
+            QStartInfusion, //double check before start infusion
+            Infusing, // infusion is ongoing
+            Paused,  // infusion is paused
+            SettingsCancelled // configuration is cancelled
+        }
+        private Status status = Status.Off;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void initialiseValue()
+        {
+            batteryPercent = 100;
+            duration = 10;
+            volume = 100;
+            duration = 10;
+            oldVolume = volume;
+            oldDuration = duration;
+
+            status = Status.Initial;
+            setDisplay();
+		}
+
+        private void setDisplay()
+        {
+            if (status == Status.Off)
+            {
+                textBoxMode.Text = "Powered Off";
+                textBoxInfo.Text = "";
+            }
+            else
+            {
+                switch (status)
+                {
+                    case Status.Off:
+                        textBoxMode.Text = "Powered Off";
+                        textBoxInfo.Text = "";
+                        break;
+                    case Status.Initial:
+                        textBoxMode.Text = "Powered On";
+                        textBoxInfo.Text = "";
+                        break;
+                    case Status.SetVolume:
+                        textBoxMode.Text = "Volume:";
+                        textBoxInfo.Text = volume + "mL";
+                        break;
+                    case Status.SetDuration:
+                        textBoxMode.Text = "Duration";
+                        textBoxInfo.Text = duration + " minutes";
+                        break;
+                    case Status.SettingsConfirmed:
+                        textBoxMode.Text = "Start Infusion?";
+                        textBoxInfo.Text = "Yes/No";
+                        break;
+                    case Status.QStartInfusion:
+                        textBoxMode.Text = "Starting Infusion...";
+                        textBoxInfo.Text = "";
+                        break;
+                    case Status.Infusing:
+                        textBoxMode.Text = "RUNNING";
+                        textBoxInfo.Text = "Infusing...";
+                        break;
+                    case Status.Paused:
+                        textBoxMode.Text = "Infusion Paused";
+                        textBoxInfo.Text = "PAUSED";
+                        break;
+                    case Status.SettingsCancelled:
+                        textBoxMode.Text = "Operation Cancelled";
+                        textBoxInfo.Text = "RESET";
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+
+                //textBoxInfo.Text = "Pump ID: " + pumpID + "\r\nBattery Percentage: " + batteryPercent + "\r\n\r\nVolume (mL): " + volume + "\r\nDuration(mins): " + duration + "\r\nRate(mL/mins): " + String.Format("%.2f", (double) volume / duration) + "\r\n\r\nStatus: " + status;
+            }
+        }
+
+
+
+		/*
+		 * Power on or power off
+		 */
+		private void executeOnOff(object sender, EventArgs e)
+		{
+			if (status == Status.Off)
+			{
+				status = Status.Initial;
+				initialiseValue();
+                buttonPlus.Enabled = true;
+                buttonMinus.Enabled = true;
+                buttonNegAns.Enabled = true;
+                buttonPositiveAns.Enabled = true;
+                Console.WriteLine("**** Powered on ****");
+			}
+			else
+			{
+				status = Status.Off;
+                buttonPlus.Enabled = false;
+                buttonMinus.Enabled = false;
+                buttonNegAns.Enabled = false;
+                buttonPositiveAns.Enabled = false;
+				Console.WriteLine("**** Powered off ****");
+			}
+			setDisplay();
+		}
+
+        private void buttonPositiveAns_Click(object sender, EventArgs e)
+        {
+            switch (status)
+            {
+                case Status.Initial:
+                case Status.Infusing:
+                case Status.SettingsCancelled:
+                    status = Status.SetVolume;
+                    Console.WriteLine("**** Set volume ****");
+                    break;
+                case Status.SetVolume:
+                    status = Status.SetDuration;
+                    Console.WriteLine("**** Set duration ****");
+                    break;
+                case Status.SetDuration:
+                    status = Status.SettingsConfirmed;
+                    oldVolume = volume;
+                    oldDuration = duration;
+                    Console.WriteLine("**** Confirm settings ****");
+                    break;
+                case Status.SettingsConfirmed:
+                    status = Status.Infusing;
+                    Console.WriteLine("**** Start infusion ? ****");
+                    break;
+                case Status.QStartInfusion:
+                case Status.Paused:
+                    status = Status.Infusing;
+                    Console.WriteLine("**** Start infusion ... ****");
+                    break;
+                default:
+                    break;
+            }
+            setDisplay();
+		}
+
+        private void buttonNegAns_Click(object sender, EventArgs e)
+        {
+            switch (status)
+            {
+                case Status.SetVolume:
+                case Status.SettingsConfirmed:
+                case Status.SetDuration:
+                    status = Status.SettingsCancelled;
+                    volume = oldVolume;
+                    duration = oldDuration;
+                    Console.WriteLine("**** Cancel settings ****");
+                    break;
+                case Status.Paused:
+                case Status.SettingsCancelled:
+                    initialiseValue();
+                    Console.WriteLine("**** Status: paused/settingsCancelled -> Initial ****");
+                    break;
+                case Status.Initial:
+                    Console.WriteLine("Do Nothing");
+                    break;
+                default:
+                    status = Status.Paused;
+                    Console.WriteLine("**** Status: infusing -> paused ****");
+                    break;
+            }
+            setDisplay();
+		}
+
+        private void buttonMinus_Click(object sender, EventArgs e)
+        {
+            if (status == Status.SetVolume)
+            {
+                if (volume > VOLUME_MIN)
+                {
+                    volume -= VOLUME_STEP;
+                }
+                else
+                {
+                    volume = VOLUME_MIN;
+                }
+                Console.WriteLine("Decrease volume to: " + volume);
+            }
+            else if (status == Status.SetDuration)
+            {
+                if (duration > DURATION_MIN)
+                {
+                    duration -= DURATION_STEP;
+                }
+                else
+                {
+                    duration = DURATION_MIN;
+                }
+                Console.WriteLine("Decrease duration to: " + duration);
+            }
+            setDisplay();
+        }
+
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+            if (status == Status.SetVolume)
+            {
+                if (volume < VOLUME_MAX)
+                {
+                    volume += VOLUME_STEP;
+                }
+                else
+                {
+                    volume = VOLUME_MAX;
+                }
+                Console.WriteLine("Increase volume to: " + volume);
+            }
+            else if (status == Status.SetDuration)
+            {
+                if (duration < DURATION_MAX)
+                {
+                    duration += DURATION_STEP;
+                }
+                else
+                {
+                    duration = DURATION_MAX;
+                }
+                Console.WriteLine("Increase duration to: " + duration);
+            }
+            setDisplay();
+        }
+    }
+}
